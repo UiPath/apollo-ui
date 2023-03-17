ae1279540c6ab61f5582153329c336c3b754fb21
Calina Cristian <cristian.calina@uipath.com>
2023-03-16 23:51:38 -0700

    fix(apollo-react): toggle switch colors [PLT-3176] (#1641)
    * fix(apollo-react): change switch colors
    
    * fix(apollo-material-ui-theme): change switch colors
    
    * fix(apollo-react):: change switch colors

--- packages/apollo-react/src/material/theme/MuiSwitch.ts
+++ packages/apollo-react/src/material/theme/MuiSwitch.ts
@@ -5,9 +5,21 @@ export const MuiSwitch = (palette: Palette): ComponentsOverrides['MuiSwitch'] =>
     return {
         root: {
             '& .MuiSwitch-track': {
-                backgroundColor: palette.semantic.colorBackgroundGrayEmp,
+                backgroundColor: palette.semantic.colorToggleTrackOff,
                 opacity: 1,
             },
+            '& .MuiSwitch-thumb': { backgroundColor: palette.semantic.colorToggleThumbOff },
+
+            '& .MuiSwitch-switchBase.Mui-disabled + .MuiSwitch-track': { backgroundColor: palette.semantic.colorToggleTrackOffDisabled },
+            '& .MuiSwitch-switchBase.Mui-disabled .MuiSwitch-thumb': { backgroundColor: palette.semantic.colorToggleThumbOffDisabled },
+
+            '& .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track': { backgroundColor: palette.semantic.colorToggleTrackOn },
+            '& .MuiSwitch-switchBase.Mui-checked .MuiSwitch-thumb': { backgroundColor: palette.semantic.colorToggleThumbOn },
+
+            // eslint-disable-next-line max-len
+            '& .MuiSwitch-switchBase.Mui-checked.Mui-disabled + .MuiSwitch-track': { backgroundColor: palette.semantic.colorToggleTrackOnDisabled },
+            // eslint-disable-next-line max-len
+            '& .MuiSwitch-switchBase.Mui-checked.Mui-disabled .MuiSwitch-thumb': { backgroundColor: palette.semantic.colorToggleThumbOnDisabled },
         },
     };
 };
