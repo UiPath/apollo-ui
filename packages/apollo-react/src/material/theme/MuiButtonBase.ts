7fb4d0c9f2d41d1c3587a0babf7d06d30adde33e
Kitty Li <52163182+kittyyueli@users.noreply.github.com>
2023-04-19 19:46:14 -0700

    fix(apollo-react): react fixes (#1708)
    couple of react theming fixes

--- packages/apollo-react/src/material/theme/MuiButtonBase.ts
+++ packages/apollo-react/src/material/theme/MuiButtonBase.ts
@@ -1,5 +1,14 @@
 import { ComponentsOverrides } from '@mui/material/styles/overrides';
+import { Palette } from '@uipath/apollo-core';
 
-export const MuiButtonBase = (): ComponentsOverrides['MuiButtonBase'] => {
-    return { root: { '&:focus-visible': { outline: 'revert' } } };
+export const MuiButtonBase = (palette: Palette): ComponentsOverrides['MuiButtonBase'] => {
+    return {
+        root: {
+            '&:focus-visible': {
+                outline: 'revert',
+                backgroundColor: palette.semantic.colorIconButtonFocus,
+            },
+            '&:active': { backgroundColor: palette.semantic.colorIconButtonPressed },
+        },
+    };
 };
