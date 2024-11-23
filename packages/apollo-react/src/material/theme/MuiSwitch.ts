be57699eba7251eb51f537f7cde556897603eccc
Cristian Calina <cristian.calina@uipath.com>
2024-11-22 18:51:29 -0800

    fix(apollo-react):: toggle switch fixes

--- packages/apollo-react/src/material/theme/MuiSwitch.ts
+++ packages/apollo-react/src/material/theme/MuiSwitch.ts
@@ -3,40 +3,55 @@ import type { Palette } from '@uipath/apollo-core/lib/jss/palette';
 
 export const MuiSwitch = (palette: Palette): ComponentsOverrides['MuiSwitch'] => ({
     root: {
+        width: '40px',
+        height: '20px',
+        padding: '0',
+        borderRadius: '10px',
+        marginRight: '8px',
+        '&:has(.Mui-focusVisible)': {
+            outline: `1px solid ${palette.semantic.colorFocusIndicator}`,
+            outlineOffset: '1px',
+        },
         '& .MuiSwitch-switchBase': {
+            padding: '0',
             '&.Mui-focusVisible': {
-                outline: `${palette.semantic.colorToggleThumbOff} solid 2px`,
-                backgroundColor: palette.semantic.colorToggleOffFocus,
+                outline: 'unset',
+                backgroundColor: 'unset',
             },
-            '&:hover': { backgroundColor: palette.semantic.colorToggleOffHover },
-            '&:active': { backgroundColor: palette.semantic.colorToggleOffPressed },
             '&.Mui-checked': {
-                '&:hover': { backgroundColor: palette.semantic.colorToggleOnHover },
-                '&:active': { backgroundColor: palette.semantic.colorToggleOnPressed },
                 '&.Mui-focusVisible': {
-                    outline: `${palette.semantic.colorToggleThumbOn} solid 2px`,
-                    backgroundColor: palette.semantic.colorToggleOnFocus,
+                    outline: 'unset',
+                    backgroundColor: 'unset',
                 },
             },
         },
         '& .MuiSwitch-track': {
-            backgroundColor: palette.semantic.colorToggleTrackOff,
+            backgroundColor: palette.semantic.colorForegroundDeEmp,
             opacity: 1,
         },
-        '& .MuiSwitch-thumb': { backgroundColor: palette.semantic.colorToggleThumbOff },
-
+        '& .MuiSwitch-thumb': {
+            backgroundColor: palette.semantic.colorBackground,
+            height: '14px',
+            width: '14px',
+            borderRadius: '10px',
+            position: 'relative',
+            top: '3px',
+            left: '3px',
+            boxShadow: 'unset',
+        },
         '& .MuiSwitch-switchBase.Mui-disabled + .MuiSwitch-track': {
             backgroundColor: palette.semantic.colorToggleTrackOffDisabled,
             opacity: 1,
         },
-        '& .MuiSwitch-switchBase.Mui-disabled .MuiSwitch-thumb': { backgroundColor: palette.semantic.colorToggleThumbOffDisabled },
-
-        '& .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track': { backgroundColor: palette.semantic.colorToggleTrackOn },
-        '& .MuiSwitch-switchBase.Mui-checked .MuiSwitch-thumb': { backgroundColor: palette.semantic.colorToggleThumbOn },
-
+        '& .MuiSwitch-switchBase.Mui-disabled .MuiSwitch-thumb': { backgroundColor: palette.semantic.colorForegroundDisable },
+        '& .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track': {
+            backgroundColor: palette.semantic.colorToggleThumbOn,
+            opacity: 1,
+        },
+        '& .MuiSwitch-switchBase.Mui-checked .MuiSwitch-thumb': { backgroundColor: palette.semantic.colorBackground },
         // eslint-disable-next-line max-len
-        '& .MuiSwitch-switchBase.Mui-checked.Mui-disabled + .MuiSwitch-track': { backgroundColor: palette.semantic.colorToggleTrackOnDisabled },
+        '& .MuiSwitch-switchBase.Mui-checked.Mui-disabled + .MuiSwitch-track': { backgroundColor: palette.semantic.colorBackgroundDisabled },
         // eslint-disable-next-line max-len
-        '& .MuiSwitch-switchBase.Mui-checked.Mui-disabled .MuiSwitch-thumb': { backgroundColor: palette.semantic.colorToggleThumbOnDisabled },
+        '& .MuiSwitch-switchBase.Mui-checked.Mui-disabled .MuiSwitch-thumb': { backgroundColor: palette.semantic.colorForegroundDisable },
     },
 });
