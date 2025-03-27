24f20dbb6cc533b4b7799b44a3bccb98071c14f3
Cristian Calina <cristian.calina@uipath.com>
2025-03-26 17:53:56 -0700

    chore(apollo-react): split attachment item from input

--- packages/apollo-react/src/material/components/ap-chat/components/input/chat-input-attachments.react.tsx
+++ packages/apollo-react/src/material/components/ap-chat/components/input/chat-input-attachments.react.tsx
@@ -3,17 +3,11 @@
 
 import { styled } from '@mui/material';
 import token from '@uipath/apollo-core/lib';
-import {
-    AutopilotChatFileInfo,
-    AutopilotChatFileType,
-} from '@uipath/portal-shell-util';
-// eslint-disable-next-line unused-imports/no-unused-imports
+import { AutopilotChatFileType } from '@uipath/portal-shell-util';
 import React from 'react';
 
-import { t } from '../../../../utils/localization/loc';
-import { ApTooltipReact } from '../../../ap-tooltip/ap-tooltip.react';
 import { useAttachments } from '../../providers/attachements-provider.react';
-import { AutopilotChatActionButton } from '../common/action-button.react';
+import { Attachment } from './chat-input-attachments-item.react';
 
 const AttachementsContainer = styled('div')(() => ({
     display: 'flex',
@@ -48,146 +42,6 @@ export const AttachmentIcon = styled('span')<{ fileType?: AutopilotChatFileType;
         },
     }),
 }));
-
-const StyledAttachment = styled('div')<{ showRemoveIcon: boolean }>(({
-    theme, showRemoveIcon,
-}) => ({
-    display: 'flex',
-    alignItems: 'center',
-    gap: token.Spacing.SpacingXs,
-    padding: token.Spacing.SpacingMicro,
-    margin: `${token.Spacing.SpacingMicro} 0`,
-    boxSizing: 'border-box',
-    borderRadius: token.Border.BorderRadiusL,
-    border: `${token.Border.BorderThickS} solid ${theme.palette.semantic.colorBorderDeEmp}`,
-    position: 'relative',
-    maxWidth: `calc(50% - ${token.Spacing.SpacingXs} / 2)`,
-
-    '& .attachment-name': {
-        textOverflow: 'ellipsis',
-        overflow: 'hidden',
-        whiteSpace: 'nowrap',
-        color: theme.palette.semantic.colorForeground,
-    },
-
-    '& .attachment-remove': {
-        opacity: showRemoveIcon ? 1 : 0,
-        marginLeft: 'auto',
-        position: 'absolute',
-        right: token.Spacing.SpacingMicro,
-        backgroundColor: theme.palette.semantic.colorBackground,
-        height: token.Spacing.SpacingM,
-        display: 'flex',
-
-        '& .MuiButtonBase-root': {
-            height: token.Spacing.SpacingM,
-            width: token.Spacing.SpacingM,
-            padding: 0,
-        },
-    },
-}));
-
-interface AttachmentProps {
-    attachment: AutopilotChatFileInfo;
-    onRemove: (name: string) => void;
-    setFocusedAttachmentIndex: () => void;
-    shouldFocus: boolean;
-}
-
-const Attachment = React.memo(({
-    attachment, onRemove, setFocusedAttachmentIndex, shouldFocus,
-}: AttachmentProps) => {
-    const removeButtonRef = React.useRef<HTMLButtonElement>(null);
-    const [ isRemoveIconVisible, setIsRemoveIconVisible ] = React.useState(false);
-    const [ isFocused, setIsFocused ] = React.useState(shouldFocus);
-    const attachmentRef = React.useRef<HTMLDivElement>(null);
-    const lastMousePosition = React.useRef({
-        x: 0,
-        y: 0,
-    });
-
-    // Tooltip interferes with the onMouseEnter/onMouseLeave events, so we need to listen to mouse move events
-    React.useEffect(() => {
-        const handleMouseMove = (e: MouseEvent) => {
-            if (isFocused) {
-                return;
-            }
-
-            // Store the last known mouse position
-            lastMousePosition.current = {
-                x: e.clientX,
-                y: e.clientY,
-            };
-
-            if (attachmentRef.current) {
-                const rect = attachmentRef.current.getBoundingClientRect();
-                const isInside = (
-                    e.clientX >= rect.left &&
-                    e.clientX <= rect.right &&
-                    e.clientY >= rect.top &&
-                    e.clientY <= rect.bottom
-                );
-
-                setIsRemoveIconVisible(isInside);
-            }
-        };
-
-        document.addEventListener('mousemove', handleMouseMove);
-
-        return () => document.removeEventListener('mousemove', handleMouseMove);
-    }, [ isFocused ]);
-
-    React.useEffect(() => {
-        if (shouldFocus && removeButtonRef.current) {
-            removeButtonRef.current.focus();
-        }
-    }, [ shouldFocus ]);
-
-    return (
-        <StyledAttachment
-            ref={attachmentRef}
-            id={attachment.name}
-            showRemoveIcon={isRemoveIconVisible || isFocused || shouldFocus}
-        >
-            <AttachmentIcon
-                fileType={attachment.friendlyType}
-                dangerouslySetInnerHTML={{ __html: attachment.icon }}
-            />
-
-            <ApTooltipReact
-                content={attachment.name}
-                placement="top"
-            >
-                <ap-typography class="attachment-name">{attachment.name}</ap-typography>
-            </ApTooltipReact>
-
-            <div className="attachment-remove">
-                <AutopilotChatActionButton
-                    ref={removeButtonRef}
-                    onFocus={() => setIsFocused(true)}
-                    onBlur={() => setIsFocused(false)}
-                    iconSize={token.Icon.IconS}
-                    iconName="close"
-                    tooltip={t('autopilot-chat-remove-file')}
-                    ariaLabel={t('autopilot-chat-remove-file-name', { name: attachment.name })}
-                    onClick={() => {
-                        onRemove(attachment.name);
-                        setFocusedAttachmentIndex();
-                        // Wait for next paint to ensure container has updated
-                        // and trigger mousemove event to check if the remove icon is visible
-                        requestAnimationFrame(() => {
-                            const event = new MouseEvent('mousemove', {
-                                clientX: lastMousePosition.current.x,
-                                clientY: lastMousePosition.current.y,
-                            });
-                            document.dispatchEvent(event);
-                        });
-                    }}
-                />
-            </div>
-        </StyledAttachment>
-    );
-});
 
 function AutopilotChatInputAttachmentsComponent() {
     const {
