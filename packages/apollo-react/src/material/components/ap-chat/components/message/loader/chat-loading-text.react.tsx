0533da048a9200bac1e28696fbaf7ebe8e39ec94
Shannon Suhendra <shannon@suhendra.com>
2025-04-14 16:53:41 -0400

    feat(apollo-react): customize loading messages in autopilot chat

--- packages/apollo-react/src/material/components/ap-chat/components/message/loader/chat-loading-text.react.tsx
+++ packages/apollo-react/src/material/components/ap-chat/components/message/loader/chat-loading-text.react.tsx
@@ -2,22 +2,45 @@
 /** @jsxFrag React.Fragment */
 
 import { Fade } from '@mui/material';
+import { AutopilotChatEvent } from '@uipath/portal-shell-util';
 import React, {
     useEffect,
     useState,
 } from 'react';
 
 import { t } from '../../../../../utils/localization/loc';
+import { AutopilotChatService } from '../../../services/chat-service';
 
 const SECONDS = 1000;
 const FADE_DURATION = 0.5 * SECONDS;
 const MESSAGE_DURATION = 5 * SECONDS;
 
-const messages = [ 'autopilot-chat-thinking', 'autopilot-chat-analyzing', 'autopilot-chat-thinking-more' ];
+const defaultMessages = [ 'autopilot-chat-thinking', 'autopilot-chat-analyzing', 'autopilot-chat-thinking-more' ];
 
 export const LoadingMessage = () => {
     const [ messageIdx, setMessageIdx ] = useState(0);
     const [ isVisible, setIsVisible ] = useState(true);
+    const [ messages, setMessages ] = useState(defaultMessages);
+    const chatService = AutopilotChatService.Instance;
+
+    useEffect(() => {
+        if (!chatService) {
+            return;
+        }
+
+        const unsubscribeSetLoadingMessage = chatService.on(AutopilotChatEvent.SetLoadingMessage, (newMessages: string[]) => {
+            setIsVisible(false);
+            setTimeout(() => {
+                setMessages(newMessages);
+                setMessageIdx(0);
+                setIsVisible(true);
+            }, FADE_DURATION);
+        });
+
+        return () => {
+            unsubscribeSetLoadingMessage();
+        };
+    }, [ chatService ]);
 
     useEffect(() => {
         if (messageIdx >= messages.length - 1) {
@@ -38,7 +61,7 @@ export const LoadingMessage = () => {
             clearTimeout(parentTimeout);
             clearTimeout(childTimeout);
         };
-    }, [ messageIdx ]);
+    }, [ messageIdx, messages ]);
 
     return (
         <Fade in={isVisible} timeout={FADE_DURATION}>
