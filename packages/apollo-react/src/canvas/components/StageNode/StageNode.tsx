265f2d9137f96381d7eec72c9fbbd8c7ec617715
jorgeabundis15 <jorge.abundis@uipath.com>
2025-11-07 11:44:16 -0600

    fix(apollo-react): update stage node test id (#120)

--- packages/apollo-react/src/canvas/components/StageNode/StageNode.tsx
+++ packages/apollo-react/src/canvas/components/StageNode/StageNode.tsx
@@ -412,7 +412,7 @@ const StageNodeComponent = (props: StageNodeProps) => {
                       return (
                         <StageTask
                           key={task.id}
-                          data-testid={`task-${task.id}`}
+                          data-testid={`stage-task-${task.id}`}
                           selected={!!selectedTasks?.includes(task.id)}
                           status={taskExecution?.status}
                           onClick={(e) => handleTaskClick(e, task.id)}
@@ -482,7 +482,7 @@ const StageNodeComponent = (props: StageNodeProps) => {
                           {onTaskGroupModification && (
                             <StageTaskRemoveButton
                               className="task-remove-button"
-                              data-testid={`task-remove-${task.id}`}
+                              data-testid={`stage-task-remove-${task.id}`}
                               onClick={(event) => handleTaskRemove(event, groupIndex, taskIndex)}
                             >
                               <ApIcon name="close" size="16px" />
