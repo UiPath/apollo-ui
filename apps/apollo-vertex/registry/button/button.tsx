b50704fc9215e12e8f6ffb8c7603c1939eeda5b1
0xr3ngar <bogdan.nikolov4@outlook.com>
2025-11-24 11:52:47 +0200

    feat(apollo-vertex): add button component and docs

--- apps/apollo-vertex/registry/button/button.tsx
+++ apps/apollo-vertex/registry/button/button.tsx
@@ -1,21 +1,18 @@
-import * as React from "react";
+import * as React from 'react';
 
-export interface ButtonProps
-    extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-    asChild?: boolean;
+export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  asChild?: boolean;
 }
 
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-    ({ className, ...props }, ref) => {
-        return (
-            <button
-                className="inline-flex bg-red-400 items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2"
-                ref={ref}
-                {...props}
-            />
-        );
-    },
-);
-Button.displayName = "Button";
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(({ className, ...props }, ref) => {
+  return (
+    <button
+      className="inline-flex bg-red-400 items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2"
+      ref={ref}
+      {...props}
+    />
+  );
+});
+Button.displayName = 'Button';
 
 export { Button };
