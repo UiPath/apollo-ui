43eb450e672fa1d08361fd10164a77e1ff3bafbe
Ruud Andriessen <ruud.andriessen@uipath.com>
2025-12-03 14:00:20 +0100

    chore(docs): setup example template

---
+++ apps/apollo-vertex/templates/MyTemplate.tsx
@@ -0,0 +1,15 @@
+import { Button } from '@/registry/button/button';
+import { Card, CardContent, CardHeader, CardTitle } from '@/registry/card/card';
+
+export function MyTemplate() {
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle>My Template</CardTitle>
+            </CardHeader>
+            <CardContent>
+                <Button>Click me</Button>
+            </CardContent>
+        </Card>
+    );
+}
