7e63df25ac050a954bfa4231db0709203dd0be68
Cristian Calina <cristian.calina@uipath.com>
2025-12-04 12:16:53 -0800

    feat(apollo-react): force release

--- packages/apollo-react/src/core/index.ts
+++ packages/apollo-react/src/core/index.ts
@@ -1,7 +1,7 @@
 // Re-export apollo-core tokens
 export * from '@uipath/apollo-core/tokens';
 
-// Re-export icon types and names (no SVG bundles, just metadata)
+// Re-export icon types and names 
 export { type IconName, iconNames } from '@uipath/apollo-core/icons';
 
 // Re-export apollo-utils
