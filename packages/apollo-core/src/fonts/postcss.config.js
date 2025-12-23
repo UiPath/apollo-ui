7f396ae24183a649aeb0c3c37ef482cfb21241d8
Cristian Calina <cristian.calina@uipath.com>
2025-12-22 23:39:31 -0800

    chore(apollo-core): add fonts build

--- packages/apollo-core/src/fonts/postcss.config.js
+++ packages/apollo-core/src/fonts/postcss.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     plugins: [
+        // eslint-disable-next-line @typescript-eslint/no-require-imports
         require('cssnano')({
             preset: ['default', {
                 normalizeUrl: false
