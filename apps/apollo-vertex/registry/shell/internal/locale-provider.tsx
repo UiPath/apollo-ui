3624dfcc1e9d7ef9e2dc7992542cbf11a56ef731
Alin Cadariu <cadariualin@gmail.com>
2026-01-28 14:52:09 +0100

    feat(apollo-vertex): add language switcher component

---
+++ apps/apollo-vertex/registry/shell/internal/locale-provider.tsx
@@ -0,0 +1,50 @@
+import {
+  type PropsWithChildren,
+  useEffect,
+  useEffectEvent,
+  useState,
+} from "react";
+import { useTranslation } from "react-i18next";
+import { Spinner } from "@/components/ui/spinner";
+import { configurei18n } from "@/lib/i18n";
+import type { LanguageChangedEvent } from "./language-toggle";
+
+export const LocaleProviderComponent = ({ children }: PropsWithChildren) => {
+  const { i18n } = useTranslation();
+
+  const handleLanguageChanged = useEffectEvent((event: Event) => {
+    const { selectedLanguageId } = (event as CustomEvent<LanguageChangedEvent>)
+      .detail;
+    i18n.changeLanguage(selectedLanguageId);
+    document.documentElement.lang = selectedLanguageId;
+  });
+
+  useEffect(() => {
+    document.addEventListener("languageChanged", handleLanguageChanged);
+    return () =>
+      document.removeEventListener("languageChanged", handleLanguageChanged);
+  }, []);
+
+  return children;
+};
+
+export const LocaleProvider = ({ children }: PropsWithChildren) => {
+  const [isConfigured, setIsConfigured] = useState(false);
+  const configure = async () => {
+    await configurei18n();
+    setIsConfigured(true);
+  };
+
+  useEffect(() => {
+    configure();
+  }, []);
+
+  if (!isConfigured)
+    return (
+      <div className="flex items-center justify-center h-full">
+        <Spinner className="size-10 animate-spin" />
+      </div>
+    );
+
+  return <LocaleProviderComponent>{children}</LocaleProviderComponent>;
+};
--- apps/apollo-vertex/templates/ShellTemplate.tsx
+++ apps/apollo-vertex/templates/ShellTemplate.tsx
@@ -1,11 +1,14 @@
 "use client";
 
+import { LocaleProvider } from "@/registry/shell/internal/locale-provider";
 import { ShellLayout } from "@/registry/shell/internal/shell-layout";
 
 export function ShellTemplate() {
   return (
-    <ShellLayout companyName="UiPath" productName="Apollo Vertex">
-      <div />
-    </ShellLayout>
+    <LocaleProvider>
+      <ShellLayout companyName="UiPath" productName="Apollo Vertex">
+        <div />
+      </ShellLayout>
+    </LocaleProvider>
   );
 }
