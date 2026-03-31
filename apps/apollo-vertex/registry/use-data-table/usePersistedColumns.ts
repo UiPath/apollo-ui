91da0d381f38cfe9f36b336b22abbfd79618450e
Razvan Placintar <razvan.placintar@uipath.com>
2026-03-31 16:27:32 +0300

    chore(apollo-vertex): replace unmaintained @uidotdev/usehooks lib

--- apps/apollo-vertex/registry/use-data-table/usePersistedColumns.ts
+++ apps/apollo-vertex/registry/use-data-table/usePersistedColumns.ts
@@ -1,6 +1,6 @@
 "use client";
 
-import { useLocalStorage } from "@uidotdev/usehooks";
+import { useLocalStorage } from "@mantine/hooks";
 import { ENTITY_TABLE_COLUMNS_STORAGE_PREFIX } from "@/lib/constants";
 
 export interface UsePersistedColumnsProps {
@@ -16,10 +16,10 @@ export function usePersistedColumns({
 }: UsePersistedColumnsProps): [string[], (columns: string[]) => void] {
   const fullKey = `${ENTITY_TABLE_COLUMNS_STORAGE_PREFIX}${storageKey}`;
 
-  const [rawColumns, setRawColumns] = useLocalStorage<string[]>(
-    fullKey,
-    defaultColumns,
-  );
+  const [rawColumns, setRawColumns] = useLocalStorage<string[]>({
+    key: fullKey,
+    defaultValue: defaultColumns,
+  });
 
   const visibleColumns = availableColumns
     ? rawColumns.filter((key) => availableColumns.includes(key))
