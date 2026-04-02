97c85e8b83d6b8e581c8619bf903fd54cbfa36c9
Helen <59126064+Miyuki-L@users.noreply.github.com>
2026-04-02 09:54:20 -0700

    fix(apollo-react): added missing exports for stage header chips

--- packages/apollo-react/src/canvas/components/StageNode/index.ts
+++ packages/apollo-react/src/canvas/components/StageNode/index.ts
@@ -1,7 +1,9 @@
 export { StageConnectionEdge } from './StageConnectionEdge';
 export { StageEdge } from './StageEdge';
 export { StageNode } from './StageNode';
+export { StageHeaderChipType } from './StageNode.types';
 export type {
+  StageHeaderChip,
   StageNodeProps,
   StageStatus,
   StageTaskItem,
