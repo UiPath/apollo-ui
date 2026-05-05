d26805fac3ac02861535b87c14f8185a7bff0180
Kitty Li <kitty.li@uipath.com>
2026-05-05 16:50:32 -0700

    fix(apollo-react): pass through readonly
    fix pass through isReadonly

--- packages/apollo-react/src/canvas/components/StageNode/StageNodeSequentialTaskGroups.tsx
+++ packages/apollo-react/src/canvas/components/StageNode/StageNodeSequentialTaskGroups.tsx
@@ -220,6 +220,7 @@ export const StageNodeSequentialTaskGroups = ({
                           !isReadOnly && {
                             getContextMenuItems: buildContextMenuItems,
                           })}
+                        isReadOnly={isReadOnly}
                       />
                     );
                   })}
