985f941ec10306ba02471ab6b9867b7436d2bc6e
David Rios <david.rios@uipath.com>
2026-05-15 10:56:19 -0400

    chore(apollo-react): wrapper

--- packages/apollo-react/src/canvas/components/StickyNoteNode/StickyNoteNode.tsx
+++ packages/apollo-react/src/canvas/components/StickyNoteNode/StickyNoteNode.tsx
@@ -364,6 +364,7 @@ const StickyNoteNodeComponent = ({
           <TopCornerIndicators visible={selected && !readOnly} />
           <BottomCornerIndicators visible={selected && !readOnly} />
           {isEditing ? (
+            <>
               <FormattingToolbar
                 textAreaRef={textAreaRef}
                 borderColor={color}
@@ -382,6 +383,7 @@ const StickyNoteNodeComponent = ({
                 isEditing={isEditing}
                 className="nodrag nowheel"
               />
+            </>
           ) : (
             <StickyNoteMarkdown ref={markdownRef} {...scrollCaptureProps}>
               {localContent ? (
